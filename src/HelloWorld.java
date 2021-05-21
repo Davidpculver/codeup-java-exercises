309d6751e7097474f4559534303a77506d018091
David Culver <culver.david.p@gmail.com>
2021-05-21 16:30:17 -0500

    java intro

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+//  p-s-v-m method: typically used for debugging and running app inside of class; Lets us see interactions between code, and command line. The main method is like the index.html file
+    public static void main(String[] args) {
+//      System: computer - Out: output - print: print in command line. IDE compiles it for us. (command+shift+r shortcut to run)
+        System.out.print("Hello. World!");
+    }
+}
