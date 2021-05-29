0a51bf08ba90524f1c48567e99713c305c67525d
David Culver <culver.david.p@gmail.com>
2021-05-29 15:03:32 -0500

    completed java methods exercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -84,25 +84,67 @@ public class MethodsExercises {
         System.out.println("Enter the number of sides of your dice");
         int sides = scanner.nextInt();
         scanner.nextLine();
-        if(sides > 0){
+        if (sides > 0) {
             System.out.printf("You have selected a %d sided dice.%n", sides);
             System.out.println("Are you ready to roll? Y/N");
             String roll = scanner.nextLine();
-            if(roll.toLowerCase().equals("y")){
+            if (roll.toLowerCase().startsWith("y")) {
                 Random rand = new Random();
                 int die1 = rand.nextInt(sides) + 1;
-                int die2 = rand.nextInt(sides ) + 1;
+                int die2 = rand.nextInt(sides) + 1;
                 System.out.printf("Die1: %d%n", die1);
                 System.out.printf("Die2: %d%n", die2);
-            } else if(!roll.toLowerCase().equals("y")){
+            } else if (!roll.toLowerCase().startsWith("y")) {
                 System.out.println("Come back when you're ready to play. Goodbye");
             }
         } else {
             System.out.println("Please enter a valid positive number.");
             rollDice();
         }
+    }
 
 
+    public static void guessNumber() {
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0;
+        boolean correct = false;
+        Random rand = new Random();
+        int answer = rand.nextInt(100) + 1;
+        System.out.println("answer:" + answer);
+        System.out.println("Guess a number between 1 and 100");
+        do {
+            System.out.printf("You have %d attempts remaining.%n", 10 - counter);
+            if (counter == 10) {
+                System.out.println("You failed to guess the correct answer. Try again.");
+                break;
+            }
+            else if (scanner.hasNextInt()) {
+                int guess = scanner.nextInt();
+                if (guess > 100 || guess < 1) {
+                    System.out.println("You did not enter a valid guess.");
+                    System.out.printf("%nWould you like to try again? Y/N%n");
+                    String resp = scanner.next();
+                    if (resp.toLowerCase().startsWith("y")) {
+                        guessNumber();
+                    } else {
+                        System.out.println("Come back when you're ready to play. Goodbye");
+                    }
+                } else if (guess > answer) {
+                    System.out.println("LOWER");
+                    counter++;
+                } else if (guess < answer) {
+                    System.out.println("HIGHER");
+                    counter++;
+                } else {
+                    System.out.println("GOOD GUESS!!");
+                    correct = true;
+                }
+            } else if (!scanner.hasNextInt()){
+                System.out.println("You did not enter a number. Please restart and play the game the right way idiot.");
+                break;
+            }
+
+        } while (correct == false);
     }
 
 
@@ -115,6 +157,6 @@ public class MethodsExercises {
 //        getInteger(1, 10);
 //        getFactorial();
 //        rollDice();
-
+        guessNumber();
     }
 }
