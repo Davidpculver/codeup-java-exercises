7686cadb7f474f401afea765da5b7814ea191d4f
David Culver <culver.david.p@gmail.com>
2021-06-30 16:21:50 -0500

    java practice

--- src/SuperHero.java
+++ src/SuperHero.java
@@ -3,18 +3,15 @@ public class SuperHero extends Person{
 //    LECTURE ALL OVER THE PLACE. Just watched. refer to codeup github: Person and superhero
 
 
-//    private String secretIdentity;
-//    private String name;
-//
-//    public SuperHero(String name){
-//        super(name);
-//    }
-//
-//    public SuperHero(String name, String publicPerona) {
-//        super(name);
-//        this.secretIdentity = secretIdentity;
-//        this.name = name1;
-//    }
-//
-//    public String alterEgo(){}
+    private String name;
+
+    public SuperHero(String name){
+        super(name);
+    }
+
+    public SuperHero(String name, String secretIdentity) {
+        super(name);
+        this.name = name;
+    }
+
 }
